374d1e946dd46b23e4bae6239ae8a5c7145c4296
g0shell <goshatraksel@yandex.ru>
2024-11-19 17:19:58 +0300

    Complete

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set tabstop=4")
 vim.cmd("set expandtab")
+vim.cmd("set nu")
 
 -- Local files
 vim.cmd("autocmd FileType xml setlocal shiftwidth=2 softtabstop=2 expandtab")
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,22 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- Formating
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.prettier,
+                -- Diagnostics
+                null_ls.builtins.diagnostics.golangci_lint,
+                null_ls.builtins.diagnostics.semgrep,
+                null_ls.builtins.diagnostics.staticcheck,
+                -- Completions
+                null_ls.builtins.completion.luasnip,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+}
